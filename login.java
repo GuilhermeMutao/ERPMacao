28f9185c1410fab38d27e1d64e11d99f0f2cd167
MacedoMF <64197308+MacedoMF@users.noreply.github.com>
2021-10-16 16:46:47 -0300

    Add files via upload

---
+++ login.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import com.sun.xml.internal.fastinfoset.tools.StAX2SAXReader;
+import java.util.Scanner;
+/**
+ *
+ * @author Matheus
+ */
+public class login {
+        
+    public login() {
+        System.out.println("============================");
+        System.out.println("|              LOGIN       |");
+        System.out.println("|                          |");
+        System.out.println("|Digite o seu usuario..:   |");
+        Scanner login = new Scanner(System.in);
+        //validar
+        System.out.println("|Digite sua senha...:      |");
+        //valdiar
+        Scanner senha = new Scanner(System.in);
+        System.out.println("============================");
+        
+        
+    }
+}
